04851ba0205c4dc276ce9b12253a13e3b67cab21
Chris <okekechristopher981@gmail.com>
2022-10-14 07:26:51 -0700

    git programming in C for variables

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,35 +1,18 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 /**
-
- * main - returns alphabelts
-
+ * main - returns alphabets
  * Return: returns 0 (successful)
-
  **/
-
 int main(void)
-
 {
-
 	int ch = 'a';
 
-
-
 	while (ch <= 'z')
-
 	{
-
 		putchar(ch);
-
 		ch++;
-
 	}
-
 	putchar('\n');
-
 	return (0);
-
 }
